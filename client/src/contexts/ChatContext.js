3fec8dd66600dfb12c6aef3791cda71850f9302f
Marvin Orendain <marvinorendain@Marvins-MBP.home>
2020-01-01 20:34:43 -0500

    Update ChatContext; use new env variable; update logic for client-only with no fetch from backend.

--- client/src/contexts/ChatContext.js
+++ client/src/contexts/ChatContext.js
@@ -3,6 +3,9 @@ import React, { createContext, useState, useEffect } from 'react';
 export const ChatContext = createContext();
 
 const ChatContextProvider = (props) => {
+  // by default this app uses backend express server to fetch and post data
+  const [isExpress, setExpress] = useState(process.env.REACT_APP_EXPRESS_SERVER);
+
   const [users, setUsers] = useState([]);
   const [messages, setMessages] = useState([]);
 
@@ -14,36 +17,54 @@ const ChatContextProvider = (props) => {
 
   // useEffect to fetch on first render
   useEffect(() => {
-    // express api endpoint for fetching chat data
-    fetch(GET_URL)
-      .then(res => res.json())
-      .then(data => {
+    if(!isExpress) {
+        // client only with no backend api
+        const data = require('../chat.json');
         setUsers([...data.users]);
         setMessages([...data.posts]);
-        console.log('data fetched', data);
-      })
-      .catch(error => {
-        console.log('get error,', error.message);
-      });
-  }, [])
+        console.log('data imported', data);
+    } else {
+        // express api endpoint for fetching chat data
+        const fetchData = async() => {
+            try {
+                // express api endpoint for fetching chat data
+                const res = await fetch(GET_URL);
+                const data = await res.json();
+                setUsers([...data.users]);
+                setMessages([...data.posts]);
+                console.log('data fetched', data);
+            } catch (error) {
+                // error message for bad get request
+                console.log('get error,', error.message);
+            }
+        };
+        fetchData();
+      }
+  }, [isExpress]);
 
   const postMessage = (message) => {
-      fetch(POST_URL, {
-        method: 'POST',
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(message)
-      })
-      .then(res => {
-        if (res.ok) {
+      if(isExpress) {
+          fetch(POST_URL, {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(message)
+          })
+          .then(res => {
+            if (res.ok) {
+              setMessages([...messages, { ...message }])
+            } else {
+              throw new Error('Oops! Something went wrong with the fetch.');
+            }
+          })
+          .catch(error => {
+            // error message for bad get request
+            console.log('POST error:', error.message);
+          });
+      } else {
+          // fall back for client-only
           setMessages([...messages, { ...message }])
-        } else {
-          throw new Error('Oops! Something went wrong with the fetch.');
-        }
-      })
-      .catch(error => {
-        console.log('POST error:', error.message);
-      });
-  }
+      }
+  };
 
   return (
     <ChatContext.Provider value={{ users, messages, postMessage }}>
